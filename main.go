4da0ef47c5cedc198a5b615ef0047b2b5f4ad98d
Will Lehman <william.p.lehman@usace.army.mil>
2022-04-06 23:35:04 +0000

    adding a handler and a model

--- main.go
+++ main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/usace/wat-api/handler"
+	"github.com/usace/wat-api/model"
 )
 
 func main() {
-	fmt.Println("welcome to wat-api")
+	var cfg model.Config
+	if err := envconfig.Process("watapi", &cfg); err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(cfg)
+	cfg.SkipJWT = true
+
+	wHandler := handler.CreateWatHandler()
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Public Routes
+	e.GET("wat-api/version", wHandler.Version)
+	//Private Routes
+
+	log.Print("starting server on port " + cfg.AppPort)
+	log.Fatal(e.Start(":" + cfg.AppPort))
+
 }
---
+++ model/model.go
@@ -0,0 +1,6 @@
+package model
+
+type Config struct {
+	AppPort string
+	SkipJWT bool
+}

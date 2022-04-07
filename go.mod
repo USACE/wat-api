851673d8f04a385d0efad71c43169237e7396abc
Will Lehman <william.p.lehman@usace.army.mil>
2022-04-07 21:14:12 +0000

    adding some link stuff and then testing airflow

--- go.mod
+++ go.mod
@@ -8,6 +8,14 @@ require (
 )
 
 require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/oauth2 v0.0.0-20210218202405-ba52d332ba99 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
+
+require (
+	github.com/apache/airflow-client-go/airflow v0.0.0-20220302101209-92f8091a0f81
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
+	"github.com/apache/airflow-client-go/airflow"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -12,6 +14,25 @@ import (
 )
 
 func main() {
+	conf := airflow.NewConfiguration()
+	conf.Host = "localhost:8080"
+	conf.Scheme = "http"
+	cli := airflow.NewAPIClient(conf)
+
+	cred := airflow.BasicAuth{
+		UserName: "username",
+		Password: "password",
+	}
+	ctx := context.WithValue(context.Background(), airflow.ContextBasicAuth, cred)
+	fmt.Println(ctx)
+	variable, resp, err := cli.VariableApi.GetVariable(ctx, "foo").Execute()
+	fmt.Println(resp)
+	if err != nil {
+		fmt.Println("we had an error" + err.Error())
+	} else {
+		fmt.Println(variable)
+	}
+
 	var cfg config.Config
 	if err := envconfig.Process("watapi", &cfg); err != nil {
 		log.Fatal(err.Error())
@@ -30,8 +51,8 @@ func main() {
 	public.GET("wat-api/version", wHandler.Version)
 	public.GET("wat-api/plugins", wHandler.Plugins)
 	//Private Routes
-	private.POST("wat-api/compute", wHandler.Version)
-	log.Print("starting server on port " + cfg.AppPort)
-	log.Fatal(e.Start(":" + cfg.AppPort))
+	private.POST("wat-api/compute", wHandler.Version) //needs to post the job config
+	//log.Print("starting server on port " + cfg.AppPort)
+	//log.Fatal(e.Start(":" + cfg.AppPort))
 
 }

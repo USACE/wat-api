f1f85f2bde7f4b9594c5088ca039656f813f7f6b
Will Lehman <william.p.lehman@usace.army.mil>
2022-04-08 13:05:53 +0000

    attempting docker host internal

--- main.go
+++ main.go
@@ -3,19 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 
 	"github.com/apache/airflow-client-go/airflow"
-	"github.com/kelseyhightower/envconfig"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"github.com/usace/wat-api/config"
-	"github.com/usace/wat-api/handler"
 )
 
 func main() {
 	conf := airflow.NewConfiguration()
-	conf.Host = "localhost:8080"
+	conf.Host = "host.docker.internal:8080"
 	conf.Scheme = "http"
 	cli := airflow.NewAPIClient(conf)
 
@@ -23,8 +17,16 @@ func main() {
 		UserName: "username",
 		Password: "password",
 	}
+
+	foo := "foo"
+	bar := "bar"
 	ctx := context.WithValue(context.Background(), airflow.ContextBasicAuth, cred)
 	fmt.Println(ctx)
+	variable := airflow.Variable{}
+
+	variable.Key = &foo
+	variable.Value = &bar
+	cli.VariableApi.PostVariables(ctx).Variable(variable)
 	variable, resp, err := cli.VariableApi.GetVariable(ctx, "foo").Execute()
 	fmt.Println(resp)
 	if err != nil {
@@ -32,27 +34,27 @@ func main() {
 	} else {
 		fmt.Println(variable)
 	}
+	/*
+		var cfg config.Config
+		if err := envconfig.Process("watapi", &cfg); err != nil {
+			log.Fatal(err.Error())
+		}
+		fmt.Println(cfg)
+		cfg.SkipJWT = true
 
-	var cfg config.Config
-	if err := envconfig.Process("watapi", &cfg); err != nil {
-		log.Fatal(err.Error())
-	}
-	fmt.Println(cfg)
-	cfg.SkipJWT = true
+		wHandler := handler.CreateWatHandler()
+		e := echo.New()
+		private := e.Group("")
+		public := e.Group("")
+		e.Use(middleware.Logger())
+		e.Use(middleware.Recover())
 
-	wHandler := handler.CreateWatHandler()
-	e := echo.New()
-	private := e.Group("")
-	public := e.Group("")
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Public Routes
-	public.GET("wat-api/version", wHandler.Version)
-	public.GET("wat-api/plugins", wHandler.Plugins)
-	//Private Routes
-	private.POST("wat-api/compute", wHandler.Version) //needs to post the job config
-	//log.Print("starting server on port " + cfg.AppPort)
-	//log.Fatal(e.Start(":" + cfg.AppPort))
-
+		// Public Routes
+		public.GET("wat-api/version", wHandler.Version)
+		public.GET("wat-api/plugins", wHandler.Plugins)
+		//Private Routes
+		private.POST("wat-api/compute", wHandler.Version) //needs to post the job config
+		//log.Print("starting server on port " + cfg.AppPort)
+		//log.Fatal(e.Start(":" + cfg.AppPort))
+	*/
 }

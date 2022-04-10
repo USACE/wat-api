996b4c5a3a65b89eab809bb92e5b2831c7ce9376
Will Lehman <william.p.lehman@usace.army.mil>
2022-04-10 23:20:20 +0000

    generating an implementation of a job

--- wat/simulation.go
+++ wat/simulation.go
@@ -1,18 +1,27 @@
 package wat
 
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+)
+
 //Job is defined by a manifest, provisions plugin resources, sends messages
 type Job interface {
 	//manifest //shouldnt a job just serialize to json or something?
 	//provisionresources
+	ProvisionResources() error
 	//sendmessage
+	SendMessage(message string) error
 	//does this thing need to "run" or "compute"
+	Run() error
 }
 
 //DeterministicJob implements the Job interface for a Deterministic Compute
 type DeterministicJob struct {
 	//simulation name?
 	//dag
-	TimeWindow string `json:"timewindow"`
+	TimeWindow `json:"timewindow"`
 	//Outputdestination string                 `json:"outputdestination"`
 	//Inputsource       string                 `json:"inputsource"`
 }
@@ -20,7 +29,7 @@ type DeterministicJob struct {
 //StochasticJob implements the job interface for a Stochastic Simulation
 type StochasticJob struct {
 	//dag
-	TimeWindow                   string `json:"timewindow"`
+	TimeWindow                   `json:"timewindow"`
 	TotalRealizations            int    `json:"totalrealizations"`
 	EventsPerRealization         int    `json:"eventsperrealization"`
 	InitialRealizationSeed       int64  `json:"initialrealizationseed"`
@@ -28,4 +37,38 @@ type StochasticJob struct {
 	Outputdestination            string `json:"outputdestination"`
 	Inputsource                  string `json:"inputsource"`
 	DeleteOutputAfterRealization bool   `json:"delete_after_realization"`
+}
+
+func (sj StochasticJob) ProvisionResources() error {
+	fmt.Println("provisioning resources...")
+	return nil
+}
+func (sj StochasticJob) SendMessage(message string) error {
+	fmt.Println("sending message: " + message)
+	return nil
+}
+func (sj StochasticJob) Run() error {
+	eventrg := rand.New(rand.NewSource(sj.InitialEventSeed))             //Natural Variability
+	realizationrg := rand.New(rand.NewSource(sj.InitialRealizationSeed)) //KnowledgeUncertianty
+	for i := 0; i < sj.TotalRealizations; i++ {                          //knowledge uncertainty loop
+		realizationSeed := realizationrg.Int63()
+		for j := 0; j < sj.EventsPerRealization; j++ { //natural variability loop
+			//ultimately need to send messages for each task in the event (defined by the dag)
+			eventSeed := eventrg.Int63()
+			ec := EventConfiguration{
+				OutputDestination:        sj.Outputdestination,
+				RealizationNumber:        i,
+				KnowledgeUncertaintySeed: realizationSeed,
+				EventNumber:              j,
+				NaturalVariabilitySeed:   eventSeed,
+				EventTimeWindow:          sj.TimeWindow,
+			}
+			bytes, err := json.Marshal(ec)
+			if err != nil {
+				return err
+			}
+			sj.SendMessage(string(bytes))
+		}
+	}
+	return nil
 }

d4879906b7e977c0ab71964aa18f5097e2652e5f
Will Lehman <william.p.lehman@usace.army.mil>
2022-05-01 12:18:12 +0000

    updating versions

--- handlers/mockdata.go
+++ handlers/mockdata.go
@@ -9,8 +9,8 @@ import (
 
 func MockPlugins() []wat.Plugin {
 	plugins := make([]wat.Plugin, 3)
-	plugins[0] = wat.Plugin{Name: "fragilitycurveplugin", ImageAndTag: "williamlehman/fragilitycurveplugin:v0.0.3"}
-	plugins[1] = wat.Plugin{Name: "hydrograph_scaler", ImageAndTag: "williamlehman/hydrographscaler:v0.0.3"}
+	plugins[0] = wat.Plugin{Name: "fragilitycurveplugin", ImageAndTag: "williamlehman/fragilitycurveplugin:v0.0.4"}
+	plugins[1] = wat.Plugin{Name: "hydrograph_scaler", ImageAndTag: "williamlehman/hydrographscaler:v0.0.5"}
 	plugins[2] = wat.Plugin{Name: "hydrograph_stats", ImageAndTag: "tbd/hydrographstats:v0.0.2"}
 	return plugins
 }

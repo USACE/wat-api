70f2b15b0d09d4739fabc5d34511688bd818bdd9
Will Lehman <william.p.lehman@usace.army.mil>
2022-05-14 20:46:50 +0000

    updating the model manifest to contain compute resources

--- wat/model.go
+++ wat/model.go
@@ -4,6 +4,15 @@ package wat
 type ModelConfiguration struct {
 	Name                        string         `json:"model_name" yaml:"model_name"`                               //model library guid?
 	ModelConfigurationResources []ResourceInfo `json:"model_configuration_paths" yaml:"model_configuration_paths"` //probably a uri?
+	ModelComputeResources       `json:"model_compute_resources" yaml:"model_compute_resources"`
+}
+type ModelComputeResources struct {
+	MinCpus       int       `json:"min_cpus" yaml:"min_cpus"`
+	DesiredCpus   int       `json:"desired_cpus" yaml:"desired_cpus"`
+	MaxCpus       int       `json:"max_cpus" yaml:"max_cpus"`
+	InstanceTypes []*string `json:"instance_types" yaml:"instance_types"`
+	Type          *string   `json:"compute_environment_type" yaml:"compute_environment_type"`
+	Managed       bool      `json:"compute_environment_management_state" yaml:"compute_environment_management_state"`
 }
 
 //ModelManifest is defined by a set of files, provides inputs and ouptuts, is recognizable by a Model Library MCAT

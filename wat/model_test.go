4f965a64cf3c968ce30f805eb7945be3a16e3692
Will Lehman <william.p.lehman@usace.army.mil>
2022-05-27 13:04:40 +0000

    payloads and manifests for hsm, mutator and runner

--- wat/model_test.go
+++ wat/model_test.go
@@ -134,7 +134,115 @@ func TestModelPayloadSeralization(t *testing.T) {
 	t.Log(string(ybytes))
 }
 
-func TestRASModelManifestSeralization(t *testing.T) {
+func TestHSMModelManifestSeralization(t *testing.T) {
+
+	inputs := make([]Input, 0)
+	outputs := make([]Output, 3)
+	outputs[0] = Output{
+		Name:      "hsm1.csv",
+		Parameter: "flow",
+		Format:    ".csv",
+	}
+	outputs[1] = Output{
+		Name:      "hsm2.csv",
+		Parameter: "flow",
+		Format:    ".csv",
+	}
+	outputs[2] = Output{
+		Name:      "hsm3.csv",
+		Parameter: "flow",
+		Format:    ".csv",
+	}
+	paths := make([]ResourceInfo, 1)
+	paths[0] = ResourceInfo{
+		Scheme:    "https",
+		Authority: "/model-library/hsm-Test",
+		Fragment:  "hsm.json",
+	}
+
+	mc := ModelConfiguration{
+		Name:                        "hsm",
+		ModelConfigurationResources: paths,
+	}
+	var mincpus int64 = 1
+	var maxcpus int64 = 4
+	var desiredcpus int64 = 2
+	computeType := "EC2"
+	instances := make([]*string, 1)
+	instance := "m2.micro"
+	instances[0] = &instance
+	computeResoures := ModelComputeResources{
+		MinCpus:       &mincpus,
+		MaxCpus:       &maxcpus,
+		DesiredCpus:   &desiredcpus,
+		Type:          &computeType,
+		InstanceTypes: instances,
+		Managed:       true,
+	}
+	mm := ModelManifest{
+		Plugin:                Plugin{Name: "hydrographscaler", ImageAndTag: "williamlehman/hydrographscaler:v0.0.1"},
+		ModelConfiguration:    mc,
+		ModelComputeResources: computeResoures,
+		Inputs:                inputs,
+		Outputs:               outputs,
+	}
+	bytes, err := yaml.Marshal(mm)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	fmt.Println(string(bytes))
+}
+func TestHSMModelPayloadSeralization(t *testing.T) {
+	tw := TimeWindow{StartTime: time.Date(2018, 1, 1, 1, 1, 1, 1, time.Local), EndTime: time.Date(2020, time.December, 31, 1, 1, 1, 1, time.Local)}
+	event := IndexedSeed{Index: 1, Seed: 5678}
+	realization := IndexedSeed{Index: 1, Seed: 1234}
+	eventConfiguration := EventConfiguration{
+		OutputDestination: ResourceInfo{
+			Scheme:    "http",
+			Authority: "/minio/runs/realization_1/event_1",
+		},
+		Realization:     realization,
+		Event:           event,
+		EventTimeWindow: tw,
+	}
+	//someone has to make data somewhere... probably needs to be computed output
+	prevModelOutput := make([]ComputedOutput, 0)
+	outputs := make([]Output, 1)
+	outputs[0] = Output{
+		Name:      "hsm1.csv",
+		Parameter: "flow",
+		Format:    ".csv",
+	}
+	paths := make([]ResourceInfo, 1)
+	paths[0] = ResourceInfo{
+		Scheme:    "https",
+		Authority: "/model-library/hsm-Test",
+		Fragment:  "hsm.json",
+	}
+	mc := ModelConfiguration{
+		Name:                        "hsm",
+		ModelConfigurationResources: paths,
+	}
+	ml := ModelLinks{
+		LinkedInputs:     prevModelOutput,
+		NecessaryOutputs: outputs,
+	}
+	mPayload := ModelPayload{
+		TargetPlugin:       "hydrographscaler",
+		PluginImageAndTag:  "williamlehman/hydrographscaler:v0.0.1",
+		ModelConfiguration: mc,
+		ModelLinks:         ml,
+		EventConfiguration: eventConfiguration,
+	}
+	bytes, err := yaml.Marshal(mPayload)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	fmt.Println(string(bytes))
+}
+func TestRASMutatorModelManifestSeralization(t *testing.T) {
 
 	inputs := make([]Input, 1)
 	inputs[0] = Input{
@@ -217,14 +325,14 @@ func TestRASModelManifestSeralization(t *testing.T) {
 	}
 	fmt.Println(string(bytes))
 }
-func TestRASModelPayloadSeralization(t *testing.T) {
+func TestRASMutatorModelPayloadSeralization(t *testing.T) {
 	tw := TimeWindow{StartTime: time.Date(2018, 1, 1, 1, 1, 1, 1, time.Local), EndTime: time.Date(2020, time.December, 31, 1, 1, 1, 1, time.Local)}
 	event := IndexedSeed{Index: 1, Seed: 5678}
 	realization := IndexedSeed{Index: 1, Seed: 1234}
 	eventConfiguration := EventConfiguration{
 		OutputDestination: ResourceInfo{
 			Scheme:    "http",
-			Authority: "/minio/runs/",
+			Authority: "/minio/runs/realization_1/event_1",
 		},
 		Realization:     realization,
 		Event:           event,
@@ -303,6 +411,142 @@ func TestRASModelPayloadSeralization(t *testing.T) {
 	mPayload := ModelPayload{
 		TargetPlugin:       "ras-mutator",
 		PluginImageAndTag:  "williamlehman/ras-mutator:v0.0.1",
+		ModelConfiguration: mc,
+		ModelLinks:         ml,
+		EventConfiguration: eventConfiguration,
+	}
+	bytes, err := yaml.Marshal(mPayload)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	fmt.Println(string(bytes))
+}
+func TestRASRunnerModelManifestSeralization(t *testing.T) {
+	inputs := make([]Input, 0)
+	outputs := make([]Output, 2)
+	outputs[0] = Output{
+		Name:      "muncie.p04.hdf",
+		Parameter: "ras results hdf file",
+		Format:    ".hdf",
+	}
+	outputs[1] = Output{
+		Name:      "muncie.log",
+		Parameter: "ras log file",
+		Format:    ".log",
+	}
+	paths := make([]ResourceInfo, 4)
+	paths[0] = ResourceInfo{
+		Scheme:    "https",
+		Authority: "/model-library/Muncie-Test",
+		Fragment:  "muncie.p04.tmp.hdf",
+	}
+	paths[1] = ResourceInfo{
+		Scheme:    "https",
+		Authority: "/model-library/Muncie-Test",
+		Fragment:  "muncie.b04",
+	}
+	paths[2] = ResourceInfo{
+		Scheme:    "https",
+		Authority: "/model-library/Muncie-Test",
+		Fragment:  "muncie.prj",
+	}
+	paths[3] = ResourceInfo{
+		Scheme:    "https",
+		Authority: "/model-library/Muncie-Test",
+		Fragment:  "muncie.x04",
+	}
+	mc := ModelConfiguration{
+		Name:                        "Muncie",
+		ModelConfigurationResources: paths,
+	}
+	var mincpus int64 = 1
+	var maxcpus int64 = 4
+	var desiredcpus int64 = 2
+	computeType := "EC2"
+	instances := make([]*string, 1)
+	instance := "m2.micro"
+	instances[0] = &instance
+	computeResoures := ModelComputeResources{
+		MinCpus:       &mincpus,
+		MaxCpus:       &maxcpus,
+		DesiredCpus:   &desiredcpus,
+		Type:          &computeType,
+		InstanceTypes: instances,
+		Managed:       true,
+	}
+	mm := ModelManifest{
+		Plugin:                Plugin{Name: "ras-runner", ImageAndTag: "williamlehman/ras-runner:v0.0.1"},
+		ModelConfiguration:    mc,
+		ModelComputeResources: computeResoures,
+		Inputs:                inputs,
+		Outputs:               outputs,
+	}
+	bytes, err := yaml.Marshal(mm)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	fmt.Println(string(bytes))
+}
+func TestRASRunnerModelPayloadSeralization(t *testing.T) {
+	tw := TimeWindow{StartTime: time.Date(2018, 1, 1, 1, 1, 1, 1, time.Local), EndTime: time.Date(2020, time.December, 31, 1, 1, 1, 1, time.Local)}
+	event := IndexedSeed{Index: 1, Seed: 5678}
+	realization := IndexedSeed{Index: 1, Seed: 1234}
+	eventConfiguration := EventConfiguration{
+		OutputDestination: ResourceInfo{
+			Scheme:    "http",
+			Authority: "/minio/runs/realization_1/event_1",
+		},
+		Realization:     realization,
+		Event:           event,
+		EventTimeWindow: tw,
+	}
+	//someone has to make data somewhere... probably needs to be computed output
+	prevModelOutput := make([]ComputedOutput, 0)
+	outputs := make([]Output, 2)
+	outputs[0] = Output{
+		Name:      "muncie.p04.hdf",
+		Parameter: "ras results hdf file",
+		Format:    ".hdf",
+	}
+	outputs[1] = Output{
+		Name:      "muncie.log",
+		Parameter: "ras log file",
+		Format:    ".log",
+	}
+	paths := make([]ResourceInfo, 4)
+	paths[0] = ResourceInfo{
+		Scheme:    "http",
+		Authority: "/minio/runs/realization_1/event_1",
+		Fragment:  "muncie.p04.tmp.hdf",
+	}
+	paths[1] = ResourceInfo{
+		Scheme:    "http",
+		Authority: "/minio/runs/realization_1/event_1",
+		Fragment:  "muncie.b04",
+	}
+	paths[2] = ResourceInfo{
+		Scheme:    "http",
+		Authority: "/minio/runs/realization_1/event_1",
+		Fragment:  "muncie.prj",
+	}
+	paths[3] = ResourceInfo{
+		Scheme:    "http",
+		Authority: "/minio/runs/realization_1/event_1",
+		Fragment:  "muncie.x04",
+	}
+	mc := ModelConfiguration{
+		Name:                        "Muncie",
+		ModelConfigurationResources: paths,
+	}
+	ml := ModelLinks{
+		LinkedInputs:     prevModelOutput,
+		NecessaryOutputs: outputs,
+	}
+	mPayload := ModelPayload{
+		TargetPlugin:       "ras-runner",
+		PluginImageAndTag:  "williamlehman/ras-runner:v0.0.1",
 		ModelConfiguration: mc,
 		ModelLinks:         ml,
 		EventConfiguration: eventConfiguration,
